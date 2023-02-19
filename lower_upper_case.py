2e49f43f7f3dc214f159f23d8ea1c872e6b62648
Kirti <kirtirajput63969@gmail.com>
2023-02-19 21:25:54 +0530

    Conversion of words into lower and upper case.

---
+++ lower_upper_case.py
@@ -0,0 +1,7 @@
+# 5. Write a Python program that takes input from the user and displays that input back in
+# upper and lower cases.
+a = input("Enter the input...")
+b = a.lower()
+c = a.upper()
+print(b)
+print(c)
