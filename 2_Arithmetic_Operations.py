97bcdb1d9fa03ad7a4c258c5f5d71981526eae77
kirti rajput <kirtirajput63969@gmail.com>
2023-02-24 23:57:06 +0530

    Arithmetic operations

---
+++ 2_Arithmetic_Operations.py
@@ -0,0 +1,10 @@
+#Arithmetic Operations
+a = int(input("Enter value of a = "))
+b = int(input("Enter value of b = "))
+
+print("\nAddition is : ",a+b)
+print("Subtraction : ",a-b)
+print("Multiply is : ",a*b)
+print("Division is : ",a/b)
+print("Modulus is : ",a%b)
+input()
