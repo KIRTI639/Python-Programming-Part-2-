e2681230091f34988328e21277f1c1053780e6c1
kirti rajput <kirtirajput63969@gmail.com>
2023-02-27 23:25:10 +0530

    swaping of two variables and generation of random numbers

---
+++ 5_Swap_variables.py
@@ -0,0 +1,11 @@
+#Swap two variables...
+
+a = int(input("a = "))
+b = int(input("b = "))
+
+print("After Swapping :-")
+a = a+b
+b = a-b
+a = a-b
+print("a = ", a)
+print("b = ", b)
---
+++ 6_Random_Number.py
@@ -0,0 +1,5 @@
+#Generate Random number
+import random
+
+n = random.randint(0,20)
+print(n)
